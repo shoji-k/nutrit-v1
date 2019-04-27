6dee7fae02be829aa1335cd6608e5c61f1f97012
shoji-k <shoji.kobayashi.0125@gmail.com>
2019-04-27 17:19:40 +0900

    add graphs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import React, { Component, Fragment } from 'react'
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts'
 
 const nutritionData = [
   {
@@ -253,7 +262,7 @@ class RequiredNutrition extends Component {
       <Fragment>
         <h3>食事摂取基準</h3>
         <Form formValues={this.state} handleChange={this.handleChange} />
-        <hr />
+        <hr className="uk-divider-icon" />
         <List data={data[this.state.sex]} />
         <small>
           出典: <a href="https://www.glico.co.jp/navi/e07.htm">食事摂取基準</a>
@@ -280,10 +289,88 @@ const List = ({ data }) => {
   )
 }
 
+let graphData = []
+nutritionData.forEach(one => {
+  one.ages.forEach(age => {
+    const obj = {
+      age: age,
+      energy_male: one.male.energy,
+      protein_male: one.male.protein,
+      energy_female: one.female.energy,
+      protein_female: one.female.protein,
+    }
+    graphData.push(obj)
+  })
+})
+
+const RequiredNutritionGraph = () => (
+  <div className="uk-grid">
+    <div className="uk-width-1-1 uk-width-1-2@m">
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart
+          data={graphData.map(d => ({
+            age: d.age,
+            male: d.energy_male,
+            female: d.energy_female,
+          }))}
+          margin={{
+            top: 30,
+            right: 30,
+            left: 30,
+            bottom: 30,
+          }}
+        >
+          <Line type="monotone" dataKey="male" stroke="#8884d8" />
+          <Line type="monotone" dataKey="female" stroke="#cc3" />
+          <CartesianGrid stroke="#ccc" />
+          <XAxis
+            dataKey="age"
+            label={{ value: 'age', position: 'insideBottomRight', offset: 0 }}
+          />
+          <YAxis
+            label={{ value: 'Energy', angle: -90, position: 'insideLeft' }}
+          />
+          <Tooltip />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+    <div className="uk-width-1-1 uk-width-1-2@m">
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart
+          data={graphData.map(d => ({
+            age: d.age,
+            male: d.protein_male,
+            female: d.protein_female,
+          }))}
+          margin={{
+            top: 30,
+            right: 30,
+            left: 30,
+            bottom: 30,
+          }}
+        >
+          <Line type="monotone" dataKey="male" stroke="#8884d8" />
+          <Line type="monotone" dataKey="female" stroke="#cc3" />
+          <CartesianGrid stroke="#ccc" />
+          <XAxis
+            dataKey="age"
+            label={{ value: 'age', position: 'insideBottomRight', offset: 0 }}
+          />
+          <YAxis
+            label={{ value: 'Protein', angle: -90, position: 'insideLeft' }}
+          />
+          <Tooltip />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  </div>
+)
+
 const App = () => (
   <div className="uk-container">
     <h1>Nutrition</h1>
     <RequiredNutrition />
+    <RequiredNutritionGraph />
   </div>
 )
 
