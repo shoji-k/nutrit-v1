cc5a7eb5234914aa21e64c15cf22082029b54eeb
shoji-k <shoji.kobayashi.0125@gmail.com>
2019-04-27 14:36:11 +0900

    add @babel/plugin-proposal-class-properties

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,28 @@
 module.exports = {
-  "extends": [
-    "plugin:prettier/recommended",
-    "plugin:react/recommended",
-    "prettier/react"
+  extends: [
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'prettier/react',
   ],
-  "plugins": [
-    "prettier",
-    "react"
-  ],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  plugins: ['prettier', 'react'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
-  "env": {
-    "browser": true,
-    "es6": true
+  env: {
+    browser: true,
+    es6: true,
   },
-  "settings": {
-    "react": {
-      "version": "detect"
-    }
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
-  "rules": {
-    "prettier/prettier": "error",
-    "react/prop-types": 0
-  }
+  rules: {
+    'prettier/prettier': 'error',
+    'react/prop-types': 0,
+  },
 }
