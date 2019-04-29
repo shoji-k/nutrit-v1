fb02cacec237b2181011d98257e6af3e71bd4627
shoji-k <shoji.kobayashi.0125@gmail.com>
2019-04-29 19:54:48 +0900

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import RequiredNutrition from './components/RequiredNutrition/RequiredNutrition'
 import RequiredNutritionGraph from './components/RequiredNutritionGraph/RequiredNutritionGraph'
 
+const NoMatch = () => <h2>Not Found</h2>
+
 const App = () => (
-  <div className="uk-container">
+  <Router>
     <h1>Nutrition</h1>
-    <RequiredNutrition />
-    <RequiredNutritionGraph />
-  </div>
+    <div className="uk-container">
+      <Switch>
+        <Route exact path="/" component={RequiredNutrition} />
+        <Route exact path="/graph" component={RequiredNutritionGraph} />
+        <Route component={NoMatch} />
+      </Switch>
+    </div>
+  </Router>
 )
 
 export default App
