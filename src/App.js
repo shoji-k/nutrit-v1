ce295dd729e4a070180aa662c4beb4a126a5d3e0
shoji-k <shoji.kobayashi.0125@gmail.com>
2019-05-06 16:52:20 +0900

    add about page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from 'react'
+import React, { Component, Suspense, lazy } from 'react'
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,25 +11,41 @@ import ErrorBoundary from './components/ErrorBoundary'
 import Home from './components/Home'
 import Bmi from './components/Bmi/Bmi'
 import RequiredNutrition from './components/RequiredNutrition/RequiredNutrition'
+import About from './components/About'
+
 const RequiredNutritionGraph = lazy(() =>
   import('./components/RequiredNutritionGraph/RequiredNutritionGraph')
 )
 
 const NoMatch = () => <h2>Not Found</h2>
 
+class Header extends Component {
+  render() {
+    return (
+      <nav className="uk-navbar">
+        <div className="uk-navbar-left">
+          <h1 style={{ margin: 0 }}>
+            <Link to="/" className="uk-navbar-item uk-logo">
+              Nutrit
+            </Link>
+          </h1>
+        </div>
+        <div className="uk-navbar-right">
+          <ul className="uk-navbar-nav">
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}
+
 const Main = () => (
   <div className="uk-container">
-    <nav className="uk-navbar">
-      <div className="uk-navbar-left">
-        <h1 style={{ margin: 0 }}>
-          <Link to="/" className="uk-navbar-item uk-logo">
-            Nutrit
-          </Link>
-        </h1>
-      </div>
-    </nav>
-
     <ErrorBoundary>
+      <Header />
       <Suspense fallback={<div>Loading...</div>}>
         <Switch>
           <Route exact path="/" component={Home} />
@@ -40,6 +56,7 @@ const Main = () => (
             path="/nutrition-graph"
             component={RequiredNutritionGraph}
           />
+          <Route exact path="/about" component={About} />
           <Route component={NoMatch} />
         </Switch>
       </Suspense>
---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+
+const About = () => (
+  <Fragment>
+    <h3>About this site</h3>
+    <p>
+      This site is created by{' '}
+      <a href="https://twitter.com/kobayashi_shoji">@kobayashi_shoji</a> from
+      2019-05-01.
+    </p>
+    <h3>About Cookie</h3>
+    <p>This site uses cookie for google analytics.</p>
+  </Fragment>
+)
+export default About
